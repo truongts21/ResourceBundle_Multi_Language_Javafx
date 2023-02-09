c48715dc1443a9c7a5fd6370d1c2eb42503fb600
truongts21 <65950186+truongts21@users.noreply.github.com>
2023-02-09 13:50:37 +0700

    translate

---
+++ src/exel/Translate.java
@@ -0,0 +1,153 @@
+package exel;
+
+public class Translate {
+	public static final String TL_RESERVATION_TAB = "reservationTab";
+	public static final String TL_FRONT_OFFICE_TAB = "frontOfficeTab";
+	public static final String TL_HOUSEKEEPING_TAB = "housekeepingTab";
+	public static final String TL_SETTING_TAB = "settingTab";
+	public static final String TL_BOOKING_TAB = "bookingTab";
+	public static final String TL_OCCUPANCY_FORECAST_TAB = "occupancyForecastTab";
+	public static final String TL_AVAILABILITY_TAB = "availabilityTab";
+	public static final String TL_HOLDING_REPORT_TAB = "holdingReportTab";
+	public static final String TL_PRODUCTION_REPORT_TAB = "productionReportTab";
+	public static final String TL_REVENUE_FORECAST_TAB = "revenueForecastTab";
+	public static final String TL_SERIES_REPORT_TAB = "seriesReportTab";
+	public static final String TL_DAILY_PICKUP_TAB = "dailyPickupTab";
+	public static final String TL_TRANSPORTATION_LIST_TAB = "transportationListTab";
+	public static final String TL_ROOM_MAP_TAB = "roomMapTab";
+	public static final String TL_ROOM_PLAN_TAB = "roomPlanTab";
+	public static final String TL_GROUP_TAB = "groupTab";
+	public static final String TL_ARRIVAL_LIST_TAB = "arrivalListTab";
+	public static final String TL_DEPARTURE_LIST_TAB = "departureListTab";
+	public static final String TL_IN_HOUSE_TAB = "inHouseTab";
+	public static final String TL_PID_TAB = "pidTab";
+	public static final String TL_POSTING_JOURNAL_TAB = "postingJournalTab";
+	public static final String TL_DRR_TAB = "drrTab";
+	public static final String TL_FOLIO_REPORT_TAB = "folioReportTab";
+	public static final String TL_TRACES_REPORT_TAB = "tracesReportTab";
+	public static final String TL_HISTORY_REPORT_TAB = "historyReportTab";
+	public static final String TL_ROOM_TRANSACTION_TAB = "roomTransactionTab";
+	public static final String TL_REVENUE_REPORT_TAB = "revenueReportTab";
+	public static final String TL_SETUP_HOTEL_TAB = "setupHotelTab";
+	public static final String TL_SETUP_ROOM_TAB = "setupRoomTab";
+	public static final String TL_PERMISSION_TAB = "permissionTab";
+	public static final String TL_DEPARTMENT_TAB = "departmentTab";
+	public static final String TL_CHARGE_CODE_TAB = "chargeCodeTab";
+	public static final String TL_RACK_RATE_TAB = "rackRateTab";
+	public static final String TL_RATE_CODE_TAB = "rateCodeTab";
+	public static final String TL_SOURCE_GROUP_TAB = "sourceGroupTab";
+	public static final String TL_PAYMENT_TAB = "paymentTab";
+	public static final String TL_VEHICLE_TAB = "vehicleTab";
+	public static final String TL_SETUP_ACCOUNT_TAB = "setupAccountTab";
+	public static final String TL_SOURCE_TAB = "sourceTab";
+	public static final String TL_SALE_ITEM_TAB = "saleItemTab";
+	public static final String TL_TERM_CONDITIONS = "termConditions";
+	public static final String TL_OTHER_SETTING_TAB = "otherSettingTab";
+	
+//	// bk
+//	public static final String TL_NO = "no";
+//	public static final String TL_BK_NO = "bkNo";
+//	public static final String TL_TOTAL_ROOM = "totalRoom";
+//	public static final String TL_TOTAL_GUEST = "totalGuest";
+//	public static final String TL_BOOKER_NAME = "bookerName";
+//	public static final String TL_BOOKER_PHONE = "bookerPhone";
+//	public static final String TL_BOOKING_SOURCE = "bookingSource";
+//	public static final String TL_GROUP_NAME = "groupName";
+//	public static final String TL_ARRIVAL = "arrival";
+//	public static final String TL_DEPARTURE = "departure";
+//	public static final String TL_TOTAL_AMOUNT = "totalAmount";
+//	public static final String TL_DEPOSIT = "deposit";
+//	public static final String TL_BALANCE = "balance";
+//	public static final String TL_CREATE_TIME = "createTime";
+//	public static final String TL_CREATE_BY = "createBy";
+//	public static final String TL_ARRIVAL_DATE = "arrivalDate";
+//	public static final String TL_FROM_DATE = "fromDate";
+//	public static final String TL_TO_DATE = "toDate";
+//	public static final String TL_SEARCH_BY = "searchBy";
+//	public static final String TL_ADD = "add";
+//	public static final String TL_EDIT = "edit";
+//	public static final String TL_CANCEL = "cancel";
+//	
+//	//OccupancyForecast
+//	public static final String TL_DATE = "date";
+//	public static final String TL_AVAILABLE_ROOM = "availableRoom";
+//	public static final String TL_OOO = "ooo";
+//	public static final String TL_TOTAL_OCC = "totalOcc";
+//	public static final String TL_NO_OF_PAX = "noOfPax";
+//	public static final String TL_PERCENT_OCC = "percentOcc";
+//	public static final String TL_CI_ROOM = "ciRoom";
+//	public static final String TL_CI_GUEST = "ciGuest";
+//	public static final String TL_CO_ROOM = "coRoom";
+//	public static final String TL_CO_GUEST = "coGuest";
+//	public static final String TL_DEF = "def";
+//	public static final String TL_TEN = "ten";
+//	public static final String TL_SERI = "seri";
+//	public static final String TL_ROOM_REVENUE = "roomRevenue";
+//	public static final String TL_TRANSPORTATION_REVENUE = "transportationRevenue";
+//	public static final String TL_OTHER_REVENUE = "otherRevenue";
+//	public static final String TL_AVERAGE_ROOM_RATE = "averageRoomRate";
+//	public static final String TL_ROOM_REVENUE_PAR = "roomRevenuePar";
+//	public static final String TL_VIEW_REPORT = "viewReport";
+//	
+//	//Availability
+//	public static final String TL_ROOM_AVAILABILITY = "roomAvailability";
+//	public static final String TL_CODE = "code";
+//	public static final String TL_DESCRIPTION = "description";
+//	public static final String TL_TOTAL = "total";
+//	public static final String TL_TOTAL_ALL = "totalAll";
+//	public static final String TL_AVAILABLE_ROOMS = "availableRooms";
+//	public static final String TL_OUT_OF_ORDER = "outOfOrder";
+//	public static final String TL_SALEABLE_ROOMS = "saleableRooms";
+//	public static final String TL_FROM_YEAR = "fromYear";
+//	public static final String TL_TO_YEAR = "toYear";
+//	public static final String TL_MONTH = "month";
+//	//productionReport
+//	public static final String TL_PRODUCTION_REPORT = "productionReport";
+//	public static final String TL_ID_GUEST = "idGuest";
+//	public static final String TL_SOURCE_NAME = "sourceName";
+//	public static final String TL_CURRENT_YEAR = "currentYear";
+//	public static final String TL_LAST_YEAR = "lastYear";
+//	public static final String TL_NO_ROOM_NIGHT = "noRoomNight";
+//	public static final String TL_FB_AND_OTHERS = "fbAndOthers";
+//	public static final String TL_PERCENT_INCREASE = "percentIncrease";
+//	//Revenue Forecast
+//	public static final String TL_REVENUE_FORECAST = "revenueForecast";
+//	public static final String TL_PAID = "paid";
+//	public static final String TL_FOC = "foc";
+//	public static final String TL_FORECAST_ON_BOOKINGS = "forecastOnBookings";
+//	public static final String TL_ALL_SALE_MANAGER = "allSaleManager";
+//	public static final String TL_PRODUCTION_CUSTOMER_GROUP = "productionCustomerGroup";
+//	public static final String TL_SOURCE_GROUP = "sourceGroup";
+//	public static final String TL_PERCENT_ROOM_NIGHT = "percentRoomNight";
+//	
+//	//Series Report
+//	public static final String TL_NUMBER_BOOKED = "numberBooked";
+//	public static final String TL_SUCCESS = "success";
+//	public static final String TL_PERCENT_SUCCESS = "percentSuccess";
+//	public static final String TL_HOLDING_REVENUE = "holdingRevenue";
+//	public static final String TL_TODAYS_HOLDING = "todaysHolding";
+//	public static final String TL_TODAYS_OCCUPANCY_PERCENT = "todaysOccupancyPercent";
+//	public static final String TL_LAST_UPDATE = "lastUpdate";
+//	public static final String TL_LAST_UPDATE_OCCUPANCY_PERCENT = "lastUpdateOccupancyPercent";
+//	public static final String TL_PICKUP = "pickup";
+//	public static final String TL_BOOKING_NUMBER = "bookingNumber";
+//	public static final String TL_ARRIVAL_DEPARTURE = "arrivalDeparture";
+//	public static final String TL_ROOM_NIGHT = "roomNight";
+//	public static final String TL_INCREASE_DECREASE = "increaseDecrease";
+//	public static final String TL_TOTAL_REVENUE = "totalRevenue";
+//	public static final String TL_DEPART = "depart";
+//	public static final String TL_RETURN = "return";
+//	public static final String TL_ARR = "arr";
+//	public static final String TL_DEP = "dep";
+//	public static final String TL_GUESTS_NAME = "guestsName";
+//	public static final String TL_DOB = "dob";
+//	public static final String TL_GEN = "gen";
+//	public static final String TL_NAT = "nat";
+//	public static final String TL_ROOM_NO = "roomNo";
+//	public static final String TL_CAR = "car";
+//	public static final String TL_SOURCE = "source";
+//	public static final String TL_PICK_UP = "pickUp";
+//	public static final String TL_DROP_OFF = "dropOff";
+//	public static final String TL_REMARKS = "remarks";
+
+}
